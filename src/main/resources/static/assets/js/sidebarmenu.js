460150501c35aaf48aae9c0422d3cb70d6da2404
kimjaesung <aamoos@openmate.co.kr>
2023-05-24 14:14:01 +0900

    왼쪽 네비 메뉴바 active

--- src/main/resources/static/assets/js/sidebarmenu.js
+++ src/main/resources/static/assets/js/sidebarmenu.js
@@ -9,45 +9,61 @@ File: js
 // ==============================================================
 $(function () {
     "use strict";
-    var url = window.location + "";
-    var path = url.replace(
-      window.location.protocol + "//" + window.location.host + "/",
-      ""
-    );
-    var element = $("ul#sidebarnav a").filter(function () {
-      return this.href === url || this.href === path; // || url.href.indexOf(this.href) === 0;
-    });
-    element.parentsUntil(".sidebar-nav").each(function (index) {
-      if ($(this).is("li") && $(this).children("a").length !== 0) {
-        $(this).children("a").addClass("active");
-        $(this).parent("ul#sidebarnav").length === 0
-          ? $(this).addClass("active")
-          : $(this).addClass("selected");
-      } else if (!$(this).is("ul") && $(this).children("a").length === 0) {
-        $(this).addClass("selected");
-      } else if ($(this).is("ul")) {
-        $(this).addClass("in");
-      }
-    });
-  
-    element.addClass("active");
-    $("#sidebarnav a").on("click", function (e) {
 
-      if (!$(this).hasClass("active")) {
-        // hide any open menus and remove all other classes
-        $("ul", $(this).parents("ul:first")).removeClass("in");
-        $("a", $(this).parents("ul:first")).removeClass("active");
-  
-        // open our new menu and add the open class
-        $(this).next("ul").addClass("in");
-        $(this).addClass("active");
-      } else if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        $(this).parents("ul:first").removeClass("active");
-        $(this).next("ul").removeClass("in");
-      }
+    //현재 화면 path
+    let path = window.location.pathname;
+
+    $(".sidebar-link").each(function(){
+        let menuUrl =$(this).attr("href");
+        let menuUrlArr = menuUrl.split('/');
+        menuUrl = "/"+menuUrlArr[1]+"/"+menuUrlArr[2];
+
+        //현재 접속한 url이 선택한 메뉴의 url에 포함되는지 체크
+        if (path.includes(menuUrl)) {
+            $(this).parents(".sidebar-item").addClass("selected")
+        }
     });
-    $("#sidebarnav >li >a.has-arrow").on("click", function (e) {
-      e.preventDefault();
-    });
+
+//    var url = window.location + "";
+//    var path = url.replace(
+//      window.location.protocol + "//" + window.location.host + "/",
+//      ""
+//    );
+//    var element = $("ul#sidebarnav a").filter(function () {
+//      return this.href === url || this.href === path; // || url.href.indexOf(this.href) === 0;
+//    });
+//    element.parentsUntil(".sidebar-nav").each(function (index) {
+//      debugger;
+//      if ($(this).is("li") && $(this).children("a").length !== 0) {
+//        $(this).children("a").addClass("active");
+//        $(this).parent("ul#sidebarnav").length === 0
+//          ? $(this).addClass("active")
+//          : $(this).addClass("selected");
+//      } else if (!$(this).is("ul") && $(this).children("a").length === 0) {
+//        $(this).addClass("selected");
+//      } else if ($(this).is("ul")) {
+//        $(this).addClass("in");
+//      }
+//    });
+//
+//    element.addClass("active");
+//    $("#sidebarnav a").on("click", function (e) {
+//
+//      if (!$(this).hasClass("active")) {
+//        // hide any open menus and remove all other classes
+//        $("ul", $(this).parents("ul:first")).removeClass("in");
+//        $("a", $(this).parents("ul:first")).removeClass("active");
+//
+//        // open our new menu and add the open class
+//        $(this).next("ul").addClass("in");
+//        $(this).addClass("active");
+//      } else if ($(this).hasClass("active")) {
+//        $(this).removeClass("active");
+//        $(this).parents("ul:first").removeClass("active");
+//        $(this).next("ul").removeClass("in");
+//      }
+//    });
+//    $("#sidebarnav >li >a.has-arrow").on("click", function (e) {
+//      e.preventDefault();
+//    });
   });
